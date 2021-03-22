4c22f02ab2a9d55531f87f75b7c550c22343d9c6
KuzmenkoO <sawkone@ukr.net>
2021-03-23 01:50:30 +0200

    add all home work №13

--- main/java/hw_13/HttpUtil.java
+++ main/java/hw_13/HttpUtil.java
@@ -1,13 +1,19 @@
-package hw_13.taskOne;
+package hw_13;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import hw_13.taskOne.User;
+import hw_13.taskThree.TaskUser;
+import hw_13.taskTwo.PostUser;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -75,19 +81,50 @@ public class HttpUtil {
         return GSON.fromJson(response.body(), User.class);
     }
 
-    public static List<User>  getUserByUsername(String username, String url) throws IOException, InterruptedException {
-//        String urlWishUsername = String.format("%s[%s]", url, username);
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create(urlWishUsername))
-//                .GET()
-//                .build();
-//
-//
-//        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
-//        return GSON.fromJson(response.body(), User.class);
+    public static String getUserByUsername(String username, String url) throws IOException, InterruptedException {
+        String urlWishUsername = String.format("%s%s", url, username);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlWishUsername))
+                .GET()
+                .build();
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        return response.body();
+    }
 
-        List<User> all = getAllUser(url);
-        return all.stream().filter(user -> user.getUsername().equals(username))
+    public static PostUser getLastPost(String url1, String url2, int userID) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("%s%d%s", url1, userID, url2)))
+                .GET()
+                .build();
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        List<PostUser> allPost = GSON.fromJson(response.body(), new TypeToken<List<PostUser>>() {
+        }.getType());
+        return allPost.stream()
+                .max(Comparator.comparing(PostUser::getId))
+                .get();
+    }
+
+    public static Path userComments(String url1, String url2, PostUser post) throws IOException, InterruptedException {
+        String fileName = "user-" + post.getUserId() + "-post-" + post.getId() + "-comments.json";
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("%s%d%s", url1, post.getId(), url2)))
+                .GET()
+                .build();
+        HttpResponse<Path> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofFile(
+                Paths.get("src/main/resources/" + fileName)));
+        return response.body();
+    }
+
+    public static List<TaskUser> allOpenTasksUser(String url1, String url2, int idUser) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(String.format("%s%d%s", url1, idUser, url2)))
+                .GET()
+                .build();
+        HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        List<TaskUser> allTask = GSON.fromJson(response.body(), new TypeToken<List<TaskUser>>() {
+        }.getType());
+        return allTask.stream()
+                .filter(task -> !task.isCompleted())
                 .collect(Collectors.toList());
     }
 }
--- main/java/hw_13/MainTest.java
+++ main/java/hw_13/MainTest.java
@@ -1,8 +1,14 @@
 package hw_13;
 
-import hw_13.taskOne.*;
+import hw_13.taskOne.Address;
+import hw_13.taskOne.Company;
+import hw_13.taskOne.Geo;
+import hw_13.taskOne.User;
+import hw_13.taskThree.TaskUser;
+import hw_13.taskTwo.PostUser;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 public class MainTest {
@@ -10,16 +16,22 @@ public class MainTest {
     private static final User USER = createDefaultUser();
     private static final String URL_USER = String.format("%s/%d", URL_TASK_1, 5);
     private static final String URL_USER_BY_USERNAME = "https://jsonplaceholder.typicode.com/users?username=";
+    private static final String URL_POST_PART_1 = "https://jsonplaceholder.typicode.com/users/";
+    private static final String URL_POST_PART_2 = "/posts";
+    private static final String URL_COMMENTS_PART_1 = "https://jsonplaceholder.typicode.com/posts/";
+    private static final String URL_COMMENTS_PART_2 = "/comments";
+    private static final String URL_PART_1 = "https://jsonplaceholder.typicode.com/users/";
+    private static final String URL_PART_2 = "/todos";
 
     public static void main(String[] args) throws IOException, InterruptedException {
         // Завдання № 1.1
-        System.out.println("Завдання №1.1");
+        System.out.println("Завдання № 1.1");
         User createUser = HttpUtil.createNewObject(URL_TASK_1, USER);
         System.out.println("Створюєм новий обєкт: \n" + createUser);
         System.out.println();
 
         // Завдання № 1.2
-        System.out.println("Завдання №1.2");
+        System.out.println("Завдання № 1.2");
         User userUpdate = HttpUtil.getUser(URL_USER);
         System.out.println("Обєкт до зміни: \n" + userUpdate);
         userUpdate.setName("Taras Tarasov");
@@ -28,13 +40,13 @@ public class MainTest {
         System.out.println();
 
         // Завдання № 1.3
-        System.out.println("Завдання №1.3");
+        System.out.println("Завдання № 1.3");
         int status = HttpUtil.deleteUserStatus(userUpdate, URL_USER);
         System.out.println("Статус видалення: " + status);
         System.out.println();
 
         // Завдання № 1.4
-        System.out.println("Завдання №1.4\nВивід інформації про усіх користувачів:");
+        System.out.println("Завдання № 1.4\nВивід інформації про усіх користувачів:");
         List<User> allUser = HttpUtil.getAllUser(URL_TASK_1);
         for (User user : allUser) {
             System.out.println(user);
@@ -42,13 +54,26 @@ public class MainTest {
         System.out.println();
 
         // Завдання № 1.5
-        System.out.println("Завдання №1.5\nОтримуєм інформацію про користувача під ID=2");
+        System.out.println("Завдання № 1.5\nОтримуєм інформацію про користувача під ID=2");
         System.out.println(HttpUtil.getUserById(2, URL_TASK_1));
         System.out.println();
 
-        // Завдання №1.6
-        System.out.println("Завдання №1.6\nОтримуєм інформацію про користувача під ніком = Bret");
-        System.out.println(HttpUtil.getUserByUsername("Bret", URL_TASK_1));
+        // Завдання № 1.6
+        System.out.println("Завдання № 1.6\nОтримуєм інформацію про користувача під ніком = Bret");
+        System.out.println(HttpUtil.getUserByUsername("Bret", URL_USER_BY_USERNAME));
+        System.out.println();
+
+        // Завдання № 2
+        System.out.println("Завдання № 2");
+        PostUser lastPost = HttpUtil.getLastPost(URL_POST_PART_1, URL_POST_PART_2, 1);
+        Path path = HttpUtil.userComments(URL_COMMENTS_PART_1, URL_COMMENTS_PART_2, lastPost);
+        System.out.println("Коментарі користувача виведено і записано у файл за адресою: " + path);
+        System.out.println();
+
+        // Завдання № 3
+        System.out.println("Завдання № 3\nВиводим усі відкриті задачі користувача з ID - 5");
+        List<TaskUser> openTask = HttpUtil.allOpenTasksUser(URL_PART_1, URL_PART_2, 5);
+        System.out.println(openTask);
     }
 
 
